1d8e72f6b50425687127f2eb61472e677ba857a5
Soner Özaşık <sonerozasik@gmail.com>
2023-06-20 17:54:11 +0300

    pred api

--- BitirmeFastApi/main.py
+++ BitirmeFastApi/main.py
@@ -1,50 +1,17 @@
-from fastapi import FastAPI, File, UploadFile
-from PIL import Image
-import os.path
-import pathlib
+from fastapi import FastAPI
 from predict import *
+from fastapi.responses import JSONResponse
 
 app = FastAPI()
-test_image_path = 'images/'
 
-@app.get('/')
-def helloworld():
-    pred = predictClass2()
-    print(pred)
-    string = f'Bening {pred[0,0]} Malignant {pred[0,1]} Normal {pred[0,2]}' 
-    return string
+@app.get('/{imageName}')
+def helloworld(imageName:str):
+    pred = predictClass(imageName)
+    x = {
+        "benign": str(pred[0,0]),
+        "malignant": str(pred[0,1]),
+        "normal": str(pred[0,2])
+    }
 
-@app.post("/upload-mri-file/")
-async def create_upload_file(file: UploadFile):
-    completeName = get_saving_path(file)
-    try:
-        await write_image_to_directory(file, completeName)
-        predResult = predictClass(test_image_path, file.filename)
-        return predResult
-    except Exception:
-        return {"message": "There was an error uploading the file"}
-    finally:
-        file.file.close()
+    return JSONResponse(content=x)
 
-    #show_saved_image(completeName)
-
-    return {"message": f"Successfully uploaded {file.filename}"}
-
-def get_saving_path(file):
-    save_path = pathlib.Path().resolve()
-
-    if not os.path.exists('images'):
-        os.makedirs('images')
-
-    completeName = os.path.join(save_path, "images", file.filename)
-
-    return completeName
-
-async def write_image_to_directory(file, completeName):
-
-    contents = file.file.read()
-    with open(completeName, 'wb') as f:
-        f.write(contents)
-
-def show_saved_image(completeName):
-    image = Image.open(completeName)
--- BitirmeFastApi/predict.py
+++ BitirmeFastApi/predict.py
@@ -3,28 +3,10 @@ import numpy as np
 from loadModel import *
 from pred_model import *
 
-IMG_SIZE = 224
-def predictClass(image_path,name):
-    test_image = cv2.imread(image_path + name)
+IMG_SIZE = 227
+def predictClass(imageName):
+    test_image = cv2.imread(f'../BitirmeBackend/API/images/{imageName}')
     test_image = cv2.resize(test_image, (IMG_SIZE, IMG_SIZE))
-    test_image = np.expand_dims(test_image, axis=0)
-    pred = model.predict(test_image)
-    probs = [pred[0][0], pred[0][1], pred[0][2], pred[0][3]]
-    names = ['Sağlıklı', 'Çok Hafif Alzheimer', 'Hafif Alzheimer', 'Gelişmiş Alzheimer']
-    mx = probs[0]
-    k = 0
-    for i in range(4):
-        if probs[i] > mx:
-            mx = probs[i]
-            k = i
-    maxv = np.max(pred)
-    pred = names[k]
-    modelClass = PredictionModel(name=pred, value=maxv)
-    return modelClass
-
-def predictClass2():
-    test_image = cv2.imread('6.png')
-    test_image = cv2.resize(test_image, (227, 227))
     test_image = np.expand_dims(test_image, axis=0)
     pred = model.predict(test_image)
     return pred
